9716fc884b4f58b7444143830fcaffc733401752
sasano8 <sasano8@LAPTOP-PCMR5DJL-wsl.localdomain>
2022-09-03 18:15:52 +0900

    update

--- example/main.py
+++ example/main.py
@@ -3,6 +3,7 @@ import warnings
 
 import numpy as np
 import pandas as pd
+from pydantic import BaseModel
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
@@ -25,9 +26,6 @@ def eval_metrics(actual, pred):
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
-
-
-from pydantic import BaseModel
 
 
 class Params(BaseModel):
